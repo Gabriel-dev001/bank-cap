3e32738a7a07afb17f4be0792e1ad45869ffabea
Gabriel-dev001 <gabriel.developer103@gmail.com>
2025-02-21 19:16:43 -0300

    Corrigindo rebase

--- Back-end/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    SECRET_KEY = "sua_chave_secreta"
-    DEBUG = True  
-
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/bd_bankcap'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
